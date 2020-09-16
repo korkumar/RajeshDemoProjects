9b80c5b706053209ca3e8416d336f52abd1206c3
korkumar <rajeshkumar.iosdev2019@gmail.com>
2020-09-16 11:07:45 +0530

    first commit

---
+++ MVVMDesignPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDesignPattern
+//
+//  Created by Apple Mac on 16/09/20.
+//  Copyright © 2020 Apple Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
